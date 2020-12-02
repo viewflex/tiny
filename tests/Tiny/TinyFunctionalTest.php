6c844e5a6dcaf0cf8ceb340976e0f56944aecf01
Jayme <romil.jayme@genesis.comcen.com.au>
2020-12-02 15:22:25 +0800

    Changed required laravel framework to 6 and Resolved Test case

--- tests/Tiny/TinyFunctionalTest.php
+++ tests/Tiny/TinyFunctionalTest.php
@@ -19,7 +19,7 @@ class TinyFunctionalTest extends TestCase
      */
     protected $tiny;
     
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         TinyTestData::create(config('tiny.tables'));
